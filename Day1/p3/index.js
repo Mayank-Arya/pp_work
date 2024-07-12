4c1cee15e19f8faf939392712ef50337e82fb2d1
Mayank-Arya <ma8183468@gmail.com>
2024-07-12 22:37:34 +0530

    ok

---
+++ Day1/p3/index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const app = express()
+const {connection} = require('./db.js')
+app.use(express.json())
+
+
+app.get('/', (req,res) => {
+    try{
+    res.status(200).json('Hello World!')
+    }
+    catch(err){
+        console.log(err.message)
+    }
+})
+
+
+app.listen(9090, async(req,res) => {
+    try{
+        await connection
+        console.log('Connected to Database successfully!!')
+        console.log('Server running on port 9090')
+    }
+    catch(err){
+        console.log(err.message)
+    }
+})
